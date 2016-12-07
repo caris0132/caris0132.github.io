2b8c82f2fa47f32a5de75f023119313b614ec735
caris0132 <nguyenductri1h1@gmail.com>
2016-12-07 16:39:25 +0700

    fix SliderAnh ex

--- JAVASCRIPT/Bai_tap_Javascript/SliderAnh/js/javascript.js
+++ JAVASCRIPT/Bai_tap_Javascript/SliderAnh/js/javascript.js
@@ -1,24 +1,75 @@
-var index_img = 0;
-function change_image(name_img,id) {
-	var img = document.getElementById("image");
-	name_img="images/"+name_img;
-	img.style.backgroundImage = "url("+name_img+")";
-	index_img = id;
-	alert(id);
-}
-function change_image_by_btn_next () {
-	var next_index = index_img+1;
-	var name = 'index'+next_index;
-	var next_img = document.getElementsByName('index'+next_index)[0];
-	if( typeof next_img !='undefined') {
-		change_image('hinh_'+next_index+'.jpg',next_index);
+var imageList;
+var slider;
+var btnImages = new Array();
+window.onload = function  () {
+	slider = document.getElementsByClassName("sliderImages")[0];
+	slider.style.width = "500px";
+	slider.style.height = "500px";
+	getListImages();
+	slider.innerHTML = "";
+	var itemImage = document.createElement('img');
+	itemImage.className = 'item-image';
+	itemImage.setAttribute('src', 'images/hinh_1.jpg');
+	itemImage.setAttribute('index', 0);
+
+	var pre = document.createElement('div');
+	pre.className = 'preview item';
+
+	var next = document.createElement('div');
+	next.className = 'next item';
+	slider.appendChild(itemImage);
+	slider.appendChild(pre);
+	for (var i = 0; i < btnImages.length; i++) {
+		btnImages[i].onclick = function  () {
+			var index = parseInt(this.getAttribute('index'))  ;
+			itemImage.setAttribute('src', this.getAttribute('src'));
+			itemImage.setAttribute('index', index);
+		};
+		slider.appendChild(btnImages[i]);
+	};
+	slider.appendChild(next);
+
+	pre.onclick = function  () {
+		var index = parseInt(itemImage.getAttribute('index'));
+		console.log(index);
+		for (var i = 1; i < btnImages.length; i++) {
+			var src = btnImages[i-1].getAttribute('src');
+			if (btnImages[i].getAttribute('index') == index) {
+			 	itemImage.setAttribute('index', (index-1));
+				itemImage.setAttribute('src', src);
+			 } 
+		};
+
 	}
-}
-function change_image_by_btn_pre() {
-	var pre_index = index_img-1;
-	var name = 'index'+pre_index;
-	var pre_img = document.getElementsByName('index'+pre_index)[0];
-	if(typeof pre_img != 'undefined'){
-		change_image('hinh_'+pre_index+'.jpg',pre_index);
+	next.onclick = function  () {
+		var index = parseInt(itemImage.getAttribute('index'));
+		console.log(index);
+		for (var i = 0; i < btnImages.length - 1 ; i++) {
+			var src = btnImages[i+1].getAttribute('src');
+			if (btnImages[i].getAttribute('index') == index) {
+			 	itemImage.setAttribute('index', (index+1));
+				itemImage.setAttribute('src', src);
+			 } 
+		};
+
 	}
+
+
+
+
+};
+function getListImages () {
+	imageList = slider.querySelectorAll('img');
+	console.log(imageList);
+	for (var i = 0; i < imageList.length; i++) {
+		var btn = document.createElement('input');
+		btn.setAttribute('type', 'button');
+		btn.setAttribute('value', 'hinh '+(i+1));
+		btn.setAttribute('index', i);
+		btn.setAttribute('src', imageList[i].getAttribute('src'));
+		btn.className = 'item btn';
+		btnImages.push(btn);
+	};
+	console.log(btnImages);
+
 }
