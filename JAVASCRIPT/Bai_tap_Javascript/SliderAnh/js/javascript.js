8ef6fba950ef9f605bc6ec748e95547c854d5c14
caris0132 <nguyenductri1h1@gmail.com>
2017-02-17 15:55:14 +0700

    fix slider images

--- JAVASCRIPT/Bai_tap_Javascript/SliderAnh/js/javascript.js
+++ JAVASCRIPT/Bai_tap_Javascript/SliderAnh/js/javascript.js
@@ -7,18 +7,23 @@ window.onload = function  () {
 	slider.style.height = "500px";
 	getListImages();
 	slider.innerHTML = "";
+	//create image default
 	var itemImage = document.createElement('img');
 	itemImage.className = 'item-image';
 	itemImage.setAttribute('src', 'images/hinh_1.jpg');
 	itemImage.setAttribute('index', 0);
 
+	//create preview button
 	var pre = document.createElement('div');
 	pre.className = 'preview item';
 
+	//create preview button
 	var next = document.createElement('div');
 	next.className = 'next item';
 	slider.appendChild(itemImage);
 	slider.appendChild(pre);
+
+	// add event click handle for All images button
 	for (var i = 0; i < btnImages.length; i++) {
 		btnImages[i].onclick = function  () {
 			var index = parseInt(this.getAttribute('index'))  ;
@@ -29,6 +34,7 @@ window.onload = function  () {
 	};
 	slider.appendChild(next);
 
+	//handle click preview change image preview
 	pre.onclick = function  () {
 		var index = parseInt(itemImage.getAttribute('index'));
 		console.log(index);
@@ -41,6 +47,7 @@ window.onload = function  () {
 		};
 
 	}
+	//handle click preview change image next
 	next.onclick = function  () {
 		var index = parseInt(itemImage.getAttribute('index'));
 		console.log(index);
@@ -58,12 +65,14 @@ window.onload = function  () {
 
 
 };
+
+//get list images 
 function getListImages () {
 	imageList = slider.querySelectorAll('img');
 	console.log(imageList);
 	for (var i = 0; i < imageList.length; i++) {
-		var btn = document.createElement('input');
-		btn.setAttribute('type', 'button');
+		var btn = document.createElement('img');
+		//btn.setAttribute('type', 'button');
 		btn.setAttribute('value', 'hinh '+(i+1));
 		btn.setAttribute('index', i);
 		btn.setAttribute('src', imageList[i].getAttribute('src'));
